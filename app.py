c4f43d14c70f6bc1528d7bc142760b3aa9f437aa
charl <charlyst23dv@gmail.com>
2025-05-08 02:04:45 -0600

    debugging

--- app.py
+++ app.py
@@ -42,9 +42,6 @@ login_manager.login_view = 'login'
 
 def activate_migrations():
     upgrade()
-  
-with app.app_context():  
-    db.create_all()  
 
 @login_manager.user_loader
 def load_user(user_id):
